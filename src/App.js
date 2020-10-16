b9551f2eac37fbafe031ecb62d5dee457dbfc5bb
Conor Wuertz <conor.wuertz@gmail.com>
2020-10-16 10:08:55 -0700

    - handle null image list or empty list state

--- src/App.js
+++ src/App.js
@@ -8,16 +8,16 @@ class App extends React.Component {
     super(props);
     // manually create some mock data for purpose of this exercise
     // consists of a list of image urls and captions
-    this.images = [
-      {
-        url: "https://www.vanguardia.com/binrepository/716x477/0c0/0d0/none/12204/HXHF/web_colp_067627_f_big_ce_VL309205_MG19806587.jpg", 
-        caption: "The awe inspiring Nevado del Ruiz volcano of the Colombian Andes."
-      },
-      {
-        url: "https://www.eluniversal.com.mx/sites/default/files/2018/09/17/canon_sumidero_chiapas_r.jpg",
-        caption: "Cañon del Sumidero, the Grand Canyon of Mexico, located in the southern state of Chiapas."
-      }
-    ];
+    // this.images = [
+    //   {
+    //     url: "https://www.vanguardia.com/binrepository/716x477/0c0/0d0/none/12204/HXHF/web_colp_067627_f_big_ce_VL309205_MG19806587.jpg", 
+    //     caption: "The awe inspiring Nevado del Ruiz volcano of the Colombian Andes."
+    //   },
+    //   {
+    //     url: "https://www.eluniversal.com.mx/sites/default/files/2018/09/17/canon_sumidero_chiapas_r.jpg",
+    //     caption: "Cañon del Sumidero, the Grand Canyon of Mexico, located in the southern state of Chiapas."
+    //   }
+    // ];
   }
   render () {
     return (
--- src/PhotoGallery.js
+++ src/PhotoGallery.js
@@ -1,13 +1,26 @@
 import React from 'react';
+import './App.css';
 
 
 class PhotoGallery extends React.Component {
+
+    renderEmpty() {
+        return (
+            <div className={"emptyGalleryContent"}>
+                There are no photos in this gallery. Try adding passing some to the PhotoGallery component!
+            </div>
+        )
+    }
     render () {
         // sanity check, receiving images via props
         console.log(this.props.images);
+        
         return (
             <div className={"photoGallery"}>
-                <div className={"mainHeader"}/>
+                <span className={"mainHeader"}> Photo Gallery </span>
+                {   // when the images props is null or of length 0, render an empty state
+                    (!this.props.images || !this.props.images.length) && this.renderEmpty()
+                }
             </div>
         )
     }
